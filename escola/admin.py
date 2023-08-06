30c61a860b7db53b2762a4e972191588e70dc014
Thayze D'Martin Costa da Silva <thayzemartin@gmail.com>
2023-08-06 14:27:20 -0300

    Update

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
-# Register your models here.
+
+class Alunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')  # campos que queremos exibir no display do admin
+    list_display_links = ('id', 'nome')  # para que, quando quisermos alterar algum aluno, =seja possível clicarmos no ID ou no nome do aluno.
+    search_fields = ('nome',)  #  campo de busca onde possamos buscar os alunos pelo nome. Portanto, search_fields = ('nome',).
+    list_per_page = 20  # paginação na quantidade de alunos. Assim, supondo que a nossa base de dados tenha 500 alunos, eles não serão exibidos de uma só vez. Definiremos, então, em 20 por página: list_per_page = 20.
+
+
+admin.site.register(Aluno, Alunos)
+
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id', 'codigo_curso', 'descricao')
+    list_display_links = ('id', 'codigo_curso')
+    search_fields = ('codigo_curso',)
+
+
+admin.site.register(Curso, Cursos)
